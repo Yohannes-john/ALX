1fc212e788bbc03fca4584ddedaffa29310cf0a4
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-10-04 02:14:40 -1200

    Update calculator.js

--- directory/calculator.js
+++ directory/calculator.js
@@ -2,8 +2,34 @@
 function add(number1, number2) {
     return number1 + number2;
 }
-
+function add(number1, number2) {
+    return number1 - number2;
+}
+function add(number1, number2) {
+    return number1 * number2;
+}
+function add(number1, number2) {
+    return number1 / number2;
+}
 document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('subtract').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('multiply').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('divide').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = add(number1, number2);
