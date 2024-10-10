15b2775939513ab1b5e8deb976de28f8e76d1343
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-10-09 21:54:25 -1200

    Update

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer() {
+    const correctAnswer =4
+    return correctAnswer
+}
+const name=document.querySelector('quiz')
+   function userAnswer{}{
+      return value 
+}
+if (correctAnswer === userAnswer) {
+    feedback = "Correct! Well done."
+} else {
+     feedback = "That's incorrect. Try again!"
+}
+const button=documentgetElementById('submit-answer')
